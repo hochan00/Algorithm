6622b450a6308e6a4ac236ced908f29ec9a6da33
유호찬 <yhc6563@gmail.com>
2026-04-04 15:50:35 +0900

    [Silver V] Title: 크로아티아 알파벳, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
+++ 백준/Silver/2941. 크로아티아 알파벳/크로아티아 알파벳.py
@@ -1,16 +1,6 @@
 user_input = input()
 al_list = ["c=", "c-", "dz=", "d-", "lj", "nj", "s=", "z="]
-num = 0
-i = 0
 
-while i < len(user_input):
-    if(user_input[i:i+3] in al_list):
-        num += 1
-        i += 3
-    elif(user_input[i:i+2] in al_list):
-        num += 1
-        i += 2
-    else:
-        num += 1
-        i += 1
-print(num)
+for i in al_list:
+    user_input = user_input.replace(i, "*")
+print(len(user_input))
