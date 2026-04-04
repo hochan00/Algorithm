a34775eb7f20271ff1d0f6dbb1afe13f33b36318
유호찬 <yhc6563@gmail.com>
2026-04-04 19:38:07 +0900

    [Silver IV] Title: 괄호, Time: 44 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -4,16 +4,18 @@ for i in range(T):
     vps = input()
     stack = []
     is_valid = True
+
     for x in vps:
-        if x == "(":
+        if x == '(':
             stack.append(x)
         else:
             if len(stack) == 0:
                 is_valid = False
                 break
-            stack.pop()
-            
-    if len(stack) == 0 and is_valid == True:
+            else:
+                stack.pop()
+
+    if len(stack) == 0 and is_valid:
         print("YES")
     else:
         print("NO")
