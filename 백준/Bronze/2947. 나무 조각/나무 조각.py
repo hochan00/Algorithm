76d9310ff91956c60f0b5ae1eafa6abc813bc3f7
유호찬 <yhc6563@gmail.com>
2026-04-04 17:47:22 +0900

    [Bronze I] Title: 나무 조각, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/2947. 나무 조각/나무 조각.py
+++ 백준/Bronze/2947. 나무 조각/나무 조각.py
@@ -4,7 +4,4 @@ while nums != [1, 2, 3, 4, 5]:
     for i in range(0, len(nums)):
         if i<4 and nums[i] > nums[i+1]:
             nums[i], nums[i+1] = nums[i+1], nums[i]
-            for x in range(len(nums)):
-                print(nums[x], end=' ')
-            print("")
-
+            print(*nums)
