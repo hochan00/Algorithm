0204841ee9b4065a5cefc87b344da3c434a5f762
유호찬 <yhc6563@gmail.com>
2026-04-04 13:55:45 +0900

    [Bronze II] Title: 공 바꾸기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
+++ 백준/Bronze/10813. 공 바꾸기/공 바꾸기.py
@@ -2,7 +2,7 @@ N, M = [int(i) for i in input().split()]
 list_num = [i+1 for i in range(N)]
 
 for _ in range(M):
-    j, i = [int(i) for i in input().split()]
+    i, j = [int(x) for x in input().split()]
     list_num[i-1], list_num[j-1] = list_num[j-1], list_num[i-1]
 
 print(*list_num)
