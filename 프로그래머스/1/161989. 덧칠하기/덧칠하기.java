d4a9b3e0615b8c1b127bbc6afc872d1a8fbbad74
유호찬 <yhc6563@gmail.com>
2026-04-12 23:30:25 +0900

    [level 1] Title: 덧칠하기, Time: 1.33 ms, Memory: 83.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/161989. 덧칠하기/덧칠하기.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int m, int[] section) {
+        int count = 0;
+        int lastPainted = 0;
+
+        for (int s : section) {
+            if (s > lastPainted) {
+                count++;
+                lastPainted = s + m - 1;
+            }
+        }
+
+        return count;
+    }
+}
