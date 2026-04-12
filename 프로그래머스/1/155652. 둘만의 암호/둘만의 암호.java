afd765d4dbc147b238e99a931f51cea257c3b4f9
유호찬 <yhc6563@gmail.com>
2026-04-12 23:37:05 +0900

    [level 1] Title: 둘만의 암호, Time: 1.13 ms, Memory: 82.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        StringBuilder answer = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            for (int i = 0; i < index; i++) {
+                c++;
+                if (c > 'z') {
+                    c = 'a';
+                }
+                if (skip.contains(String.valueOf(c))) {
+                    i--;
+                }
+            }
+            answer.append(c);
+        }
+
+        return answer.toString();
+    }
+}
